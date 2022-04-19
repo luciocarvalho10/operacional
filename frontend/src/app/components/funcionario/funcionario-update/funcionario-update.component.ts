31c8305f1f8c6539e5b1993531a0d4ceb4af96ac
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2022-04-19 12:39:37 -0400

    criando e configurando componente delete

--- frontend/src/app/components/funcionario/funcionario-update/funcionario-update.component.ts
+++ frontend/src/app/components/funcionario/funcionario-update/funcionario-update.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IFuncionario } from '../funcionario.model';
+import { FuncionarioService } from '../funcionario.service';
 
 @Component({
   selector: 'app-funcionario-update',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FuncionarioUpdateComponent implements OnInit {
 
-  constructor() { }
+  funcionario: IFuncionario = {
+    nome:"",
+    cargo:""
+  }
+  constructor(
+    private funcionarioService: FuncionarioService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id') || ' '
+    this.funcionarioService.readById(id).subscribe(f => this.funcionario = f)
   }
 
+  updateFuncionario(): void {
+    this.funcionarioService.update(this.funcionario).subscribe(() => {
+      this.funcionarioService.showMessage('Funcionario atualizado!')
+
+      this.cancel()
+    })
+  }
+
+  cancel() {
+    this.router.navigate(['/funcionario'])
+  }
 }
