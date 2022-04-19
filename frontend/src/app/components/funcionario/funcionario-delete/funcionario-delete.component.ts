e6699bcf2eeeee9beabf3c0801f215421e8dd3d2
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2022-04-19 13:27:34 -0400

    configurando componente delete

--- frontend/src/app/components/funcionario/funcionario-delete/funcionario-delete.component.ts
+++ frontend/src/app/components/funcionario/funcionario-delete/funcionario-delete.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IFuncionario } from '../funcionario.model';
+import { FuncionarioService } from '../funcionario.service';
 
 @Component({
   selector: 'app-funcionario-delete',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./funcionario-delete.component.css']
 })
 export class FuncionarioDeleteComponent implements OnInit {
-
-  constructor() { }
+  funcionario: IFuncionario = {
+    nome: '',
+    cargo: ''
+  }
+  constructor(
+    private funcionarioService: FuncionarioService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id') || ''
+    this.funcionarioService.readById(id).subscribe(f => (this.funcionario = f))
   }
 
+  deleteFuncionario(): void {
+    const id = String(this.funcionario.id)
+    this.funcionarioService.delete(id).subscribe(() => {
+      this.funcionarioService.showMessage('Funcionario deletado!')
+
+      this.cancel()
+    })
+  }
+
+  cancel() {
+    this.router.navigate(['/funcionario'])
+  }
 }
